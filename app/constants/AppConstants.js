4fd5fe749b8532fcae75495f40260ccc021d22aa
Jason Lally <jason.lally@gmail.com>
2016-04-26 15:45:38 -0700

    add app constants

---
+++ app/constants/AppConstants.js
@@ -0,0 +1,4 @@
+export const DEBUG = (process.env.NODE_ENV !== 'production')
+export const APP_TITLE = 'Open Data Explorer'
+export const NAVIGATION_UPDATED = 'NAVIGATION_UPDATED'
+export const API_DOMAIN = 'data.sfgov.org'
