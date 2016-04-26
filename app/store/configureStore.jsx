455a8d1ae8ac9f54db4bac26cae800cc1cb6cf02
Jason Lally <jason.lally@gmail.com>
2016-04-26 15:48:48 -0700

    add store

---
+++ app/store/configureStore.jsx
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import api from '../middleware/socrata'
+import createLogger from 'redux-logger'
+import rootReducer from '../reducers'
+
+export default function configureStore (initialState) {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunkMiddleware, api, createLogger())
+  )
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers').default
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+
+  return store
+}
