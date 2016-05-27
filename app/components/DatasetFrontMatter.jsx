a79f714c7084810698ae0eaff957a290c4994954
j9recurses <janine.heiser@gmail.com>
2016-05-27 09:54:38 -0700

    more css styles

--- app/components/DatasetFrontMatter.jsx
+++ app/components/DatasetFrontMatter.jsx
@@ -11,22 +11,28 @@ class DatasetFrontMatter extends Component {
     const { name, description, id, licenseName, licenseLink, rowsUpdatedAt, publishingDepartment, apiDomain, rowCount } = this.props
     let dayUpdated = moment.unix(rowsUpdatedAt).format('MM/DD/YYYY hh:mm A')
     return (
-      <Row id='header'>
-        <Col sm={9}>
-          <h1 className = {'datasetName'}> {name}</h1>
-        </Col>
-        <Col sm={9} className={'datasetDownLoadButtons'}>
-          <DownloadLinks apiDomain={apiDomain} id={id} />
-          <Button bsStyle='primary' href={`https://dev.socrata.com/foundry/${apiDomain}/${id}`} target='_blank'>API</Button>
-          <p className={'dataSetInfoDescription'}>{description}</p>
-        </Col>
-        <Col sm={3} className={'dataSetInfo'}>
-          <b>Publishing Department:</b> {publishingDepartment}<br/>
-          <b>License:</b> <a href={licenseLink}>{licenseName}</a><br/>
-          <b>Number of Rows:</b> {numberFormat(parseInt(rowCount))}<br/>
-          <b>Data Last Updated:</b> {dayUpdated}
-        </Col>
-      </Row>
+      <div className={'container-fluid dataSetTitle'}>
+        <Row id="header">
+          <Col sm={9}>
+            <h1 className = {'datasetName'}> {name}</h1>
+          </Col>
+          <Col sm={3} className={'datasetDownLoadButtons'}>
+            <DownloadLinks apiDomain={apiDomain} id={id} />
+            <Button bsStyle='primary' href={`https://dev.socrata.com/foundry/${apiDomain}/${id}`} target='_blank'>API</Button>
+          </Col>
+        </Row>
+        <Row id='header2'>
+          <Col sm={9}>
+            <p className={'dataSetInfoDescription'}>{description}</p>
+          </Col>
+          <Col sm={3} className={'dataSetInfo'}>
+            <b>Publishing Department:</b>{publishingDepartment}<br/>
+            <b>License:</b> <a href={licenseLink}>{licenseName}</a><br/>
+            <b>Number of Rows:</b> {numberFormat(parseInt(rowCount))}<br/>
+            <b>Data Last Updated:</b> {dayUpdated}
+          </Col>
+        </Row>
+      </div>
     )
   }
 }
--- app/components/HomePage/HomePage.jsx
+++ app/components/HomePage/HomePage.jsx
@@ -6,19 +6,21 @@ class HomePage extends React.Component {
     return (
 
   <section id="jumbo-search" className={'jumbotron jumbotron-default homepage'}>
-  <div className={'container'}>
-    <h1>Find the data you need</h1>
-    <p>Search hundreds of datasets from the City and County of San Francisco. Or browse on the <a href="https://data.sfgov.org" className={'ext-sf-opendata'}>data catalog</a></p>
+    <div className={'container'}>
+      <h1>Find the data you need</h1>
+      <p>Search hundreds of datasets from the City and County of San Francisco. Or browse on the <a href="https://data.sfgov.org" className={'ext-sf-opendata'}>data catalog</a></p>
       <div className={'row'}>
-        <div className={'col-md-12'}>
-          <form action="https://data.sfgov.org/data" role="form" className={'form-inline'}>
-                <input id="search-catalog-input" className={'search-input form-control input-mysize'} type="text" name="search" placeholder="Search for data, etc." autocomplete="off" />
-                <button className={'btn btn-default btnSearch'} type="submit"><i className={'glyphicon glyphicon-search'}></i></button>
-         </form>
-        </div>
+        <form action="https://data.sfgov.org/data" role="form" className={'form-inline'}>
+          <div className={'col-md-9'}>
+            <input id="homepageSearch" className={'search-input form-control input-mysize'} type="text" name="search" placeholder="Search for data, etc."/>
+          </div>
+          <div className={'col-md-3'}>
+            <button className={'btn btn-default btnSearch'} type="submit"><i className={'glyphicon glyphicon-search'}></i></button>
+          </div>
+        </form>
       </div>
-      </div>
-    </section>
+    </div>
+  </section>
     );
   }
 }
