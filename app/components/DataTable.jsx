72f3c88fbd8d1f71092cec1bfbb4e820ae401838
Jason Lally <jason.lally@gmail.com>
2016-06-15 10:22:47 -0700

    change arrow directions on sort

--- app/components/DataTable.jsx
+++ app/components/DataTable.jsx
@@ -29,7 +29,7 @@ class SortHeaderCell extends Component {
     return (
       <Cell {...props}>
         <a onClick={this._onSortChange}>
-          {children} {sortDir ? (sortDir === SortTypes.DESC ? '↑' : '↓') : ''}
+          {children} {sortDir ? (sortDir === SortTypes.DESC ? '↓' : '↑') : ''}
         </a>
       </Cell>
     )
