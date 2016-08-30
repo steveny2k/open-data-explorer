787d79e208767c3bea0d815f26db8b1b4f2c24f3
j9recurses <janine.heiser@gmail.com>
2016-08-30 16:36:27 -0700

    more travis cl changes

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,13 +5,13 @@ set -xe
 
 if [ $TRAVIS_BRANCH == "master" ] ; then
     echo "in heeeere!!!"
+    # setup ssh agent, git config and remote
+    eval `ssh-agent -s`
+    ssh-add ~/.ssh/travis_rsa
     cd build
     # Initialize a new git repo in _site, and push it to our server.
     git init
-    # setup ssh agent, git config and remote
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@datasfexplorer.tk:/var/www/test2/"
+    git remote add deploy "deploy@datasfexplorer.tk:/var/www/test2/"
     git config user.name "Travis CI"
     git config user.email "travis@datasfexplorer.tk"
 
