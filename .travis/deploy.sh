8db9a223ffe436ece9e2afa8a0c5950ca6758633
j9recurses <janine.heiser@gmail.com>
2016-08-30 15:37:54 -0700

    more travis cl changes

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,20 +4,19 @@
 set -xe
 
 if [ $TRAVIS_BRANCH == "master" ] ; then
-    print "in heeeere!!!"
+    echo "in heeeere!!!"
     cd build
     # Initialize a new git repo in _site, and push it to our server.
     git init
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@datasfexplorer.tk:/var/www/open-data-explorer/public_html"
+    git remote add deploy "travis@datasfexplorer.tk:/var/www/test2/"
     git config user.name "Travis CI"
     git config user.email "travis@datasfexplorer.tk"
 
  # commit compressed files and push it to remote
     git add .
-    git status # debug
     git commit -m "Deploy compressed files"
     git push --force deploy master
 
