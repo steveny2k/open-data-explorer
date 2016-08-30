08efcde9b067b8750e41205cda0be621497d2ede
j9recurses <janine.heiser@gmail.com>
2016-08-30 15:12:30 -0700

    travis cl deployment stuff

--- deploy/install.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-set -x # Show the output of the following commands (useful for debugging)
-# Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_5c93ddfc62aa_key -iv $encrypted_5c93ddfc62aa_iv -in deploy/travis_rsa.enc -out ~/.ssh/travis_rsa -d
-chmod 600 ~/.ssh/travis_rsa
