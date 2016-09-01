7834aa227f680ece8077bffe600bf263406101b9
j9recurses <janine.heiser@gmail.com>
2016-08-31 21:22:36 -0700

    travis deploy changes

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,7 @@ set -x # Show the output of the following commands (useful for debugging)
 
 echo "Setting up ssh keys!"
 
-# Import the SSH deployment key
 
-openssl aes-256-cbc -K $encrypted_5c93ddfc62aa_key -iv $encrypted_5c93ddfc62aa_iv -in travis_rsa.enc -out ~/.ssh/travis_rsa -d
+# Import the SSH deployment key
+openssl aes-256-cbc -K $encrypted_5c93ddfc62aa_key -iv $encrypted_5c93ddfc62aa_iv -in .travis/travis_rsa.enc -out ~/.ssh/travis_rsa -d
 chmod 600 ~/.ssh/travis_rsa
