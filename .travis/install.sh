1926debf3f6fa1a28e98e026abca353139c97cf0
j9recurses <janine.heiser@gmail.com>
2016-08-31 22:52:44 -0700

    more tests travis ci config

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,12 +5,12 @@ echo "Setting up ssh keys!"
 
 #touch ~/.ssh/known_hosts
 #chmod 777 ~/.ssh/known_hosts
-touch ~/.ssh/config
+#touch ~/.ssh/config
 
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo -e "Host datasfexplorer.tk\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo -e "Host datasf-explorer.tk\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo -e "Host 162.243.143.214\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+#echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+#echo -e "Host datasfexplorer.tk\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+#echo -e "Host datasf-explorer.tk\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+#echo -e "Host 162.243.143.214\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_5c93ddfc62aa_key -iv $encrypted_5c93ddfc62aa_iv -in .travis/travis_rsa.enc -out ~/.ssh/travis_rsa -d
---
+++ .travis/setup_ssh_agent.sh
@@ -0,0 +1,6 @@
+#setup_ssh_agent
+#!/bin/bash
+set -x # Show the output of the following commands (useful for debugging)
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/travis_rsa
