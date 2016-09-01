ae7e19b13c89abf7b15868a7fc1bb6c650efc4ad
Jason Lally <jason.lally@gmail.com>
2016-09-01 14:01:08 -0700

    spacing fix

--- test/components/DatasetFrontMatter.spec.js
+++ test/components/DatasetFrontMatter.spec.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import DatasetFrontMatter from '../../app/components/Dataset/DatasetFrontMatter'
-import { Col } from 'react-bootstrap'
+import { Col } from 'react-boot'
 
 describe('<DatasetFrontMatter/>', () => {
 
