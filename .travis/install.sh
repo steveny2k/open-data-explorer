968bd585f643393b183a9e4e1c6de829ecead3f6
j9recurses <janine.heiser@gmail.com>
2016-09-01 17:24:02 -0700

    deployment troubles

--- .travis/install.sh
+++ .travis/install.sh
@@ -15,7 +15,7 @@ echo "Setting up ssh!"
 # Import the SSH deployment key
 
 
-openssl aes-256-cbc -K $encrypted_5c93ddfc62aa_key -iv $encrypted_5c93ddfc62aa_iv -in travis_deploy.enc -out travis_deploy -d
+openssl aes-256-cbc -K $encrypted_5c93ddfc62aa_key -iv $encrypted_5c93ddfc62aa_iv -in .travis/travis_deploy.enc -out travis_deploy -d
 rm travis_deploy.enc # Don't need it anymore
 chmod 600 travis_deploy
 mv travis_deploy ~/.ssh/id_rsa
