ab5dcd05e698bbc501d3f16577c741e1d863a29e
Jason Lally <jason.lally@gmail.com>
2016-12-07 13:21:17 -0800

    minor changes to styling

--- app/components/Loading/index.js
+++ app/components/Loading/index.js
@@ -10,19 +10,18 @@ class Loading extends Component {
         {isFetching
         ? <div className='Loading-wrapper'>
           <div className='Loading-indicator'>
-            <h3>Loading...</h3>
             <svg viewBox='0 0 32 16' width='220' fill='#476481'>
               <circle transform='translate(8 0)' cx='0' cy='8' r='0'>
                 <animate attributeName='r' values='0; 4; 0; 0' dur='1.2s' repeatCount='indefinite' begin='0'
-                  keytimes='0;0.2;0.7;1' keySplines='0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.6 0.4 0.8' calcMode='spline' />
+                  keyTimes='0;0.2;0.7;1' keySplines='0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.6 0.4 0.8' calcMode='spline' />
               </circle>
               <circle transform='translate(16 0)' cx='0' cy='8' r='0'>
                 <animate attributeName='r' values='0; 4; 0; 0' dur='1.2s' repeatCount='indefinite' begin='0.3'
-                  keytimes='0;0.2;0.7;1' keySplines='0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.6 0.4 0.8' calcMode='spline' />
+                  keyTimes='0;0.2;0.7;1' keySplines='0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.6 0.4 0.8' calcMode='spline' />
               </circle>
               <circle transform='translate(24 0)' cx='0' cy='8' r='0'>
                 <animate attributeName='r' values='0; 4; 0; 0' dur='1.2s' repeatCount='indefinite' begin='0.6'
-                  keytimes='0;0.2;0.7;1' keySplines='0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.6 0.4 0.8' calcMode='spline' />
+                  keyTimes='0;0.2;0.7;1' keySplines='0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.6 0.4 0.8' calcMode='spline' />
               </circle>
             </svg>
           </div>
