f97a8792350e0735fa4e96ffb7bd563d003abbd8
j9recurses <janine.heiser@gmail.com>
2016-12-12 11:19:48 -0800

    more deploy tests

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -35,7 +35,7 @@ elif [ $TRAVIS_BRANCH == "develop" ] ; then
  # commit compressed files and push it to remote
     git add .
     git commit -m "Deploy compressed files"
-    git
+    git branch
     git push --force deploy master
     echo "****SUCCESS: Staging build was deployed ********"
 else
